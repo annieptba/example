18b089323d7c8ef08bd945dd5974515976a4c775
annieptba <61258785+annieptba@users.noreply.github.com>
2021-09-24 14:24:34 -0400

    1stcommit

---
+++ work_test.py
@@ -0,0 +1,6 @@
+def new_func(x):
+    y = x**2/ (3 - 7)
+    return y
+
+def test_new_func():
+    assert new_func(4) == -4
---
+++ workwerk.py
@@ -0,0 +1,14 @@
+x = 11
+def new_func(x):
+    y = x**2/ (3 - 7)
+new_func(x)
+
+def test_new_func():
+    assert new_func(4) == 4
+
+
+if __name__ == '__main__':
+    foo = dict()
+    newvariable725 = 'synonym_group'
+    newvariable959 = newvariable725
+    bar = [source['_source'][newvariable725] for source in foo['hits']['hits']]
